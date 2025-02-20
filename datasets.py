dd023007b02dd8902e256b5b7b2cfa9410123343
me_me <ichshlanger2@gmail.com>
2025-02-21 02:50:40 +0300

    enh: DriveAural1 and DriveAural2 datasets are finished

--- datasets.py
+++ datasets.py
@@ -1,6 +1,9 @@
 import numpy
 import os, sys
 import urllib
+import requests
+import gdown
+import mne
 
 
 class Dataset:
@@ -9,7 +12,7 @@ class Dataset:
 
 
 class FourClassMotorImagery(Dataset):
-  def __init__(self):
+  def __init__(self, output_dir:str)->None:
     self.data = numpy.array([])
 
     '''
@@ -19,15 +22,33 @@ class FourClassMotorImagery(Dataset):
 
 
 class TwoClassMotorImagery(Dataset):
-  def __init__(self):
+  def __init__(self, output_dir:str)->None:
     self.data = numpy.array([])
 
 
 class DriveAural1(Dataset):
-  def __init__(self):
-    self.data = numpy.array([])
+  def __init__(self, output_dir:str)->None:
+    url:str = 'https://drive.google.com/uc?id=1Jtx6_9s_WtJ4iUJj-d0v_BjJLNhktTC5'
+    output_filename:str = 'aural_eeg.edf'
+
+    if not os.path.exists(output_dir):
+      os.makedirs(output_dir)
+    if not os.path.isfile(os.path.join(output_dir, output_filename)):
+      gdown.download(url, os.path.join(output_dir, output_filename), quiet=False)
+
+    raw_data:mne.io.Raw = mne.io.read_raw_edf(os.path.join(output_dir, output_filename))
+    self.data, _ = raw_data[:]
 
 
 class DriveAural2(Dataset):
-  def __init__(self):
-    self.data = numpy.array([])
+  def __init__(self, output_dir:str)->None:
+    url:str = 'https://drive.google.com/uc?id=1bHjmpVouQXIxdsGYvNNv73JvDugYiaXi'
+    output_filename:str = 'aural2_eeg.edf'
+
+    if not os.path.exists(output_dir):
+      os.makedirs(output_dir)
+    if not os.path.isfile(os.path.join(output_dir, output_filename)):
+      gdown.download(url, os.path.join(output_dir, output_filename), quiet=False)
+
+    raw_data:mne.io.Raw = mne.io.read_raw_edf(os.path.join(output_dir, output_filename))
+    self.data, _ = raw_data[:]
